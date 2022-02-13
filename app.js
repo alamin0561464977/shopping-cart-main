abfc606daae682b174bf6053e1c280ec40fb51fe
alamin0561464977 <amin23435678901315@gmail.com>
2022-02-13 15:14:10 +0300

    updateProductNumber

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
+// =========iPhone 11 Silicone Case $$ iPhone=============
+function updateProductNumber(product, price, isIncreaSing) {
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
+    if (isIncreaSing == true) {
+        productNumber = parseInt(productNumber) + 1;
+    }
+    else if (productNumber > 0) {
+        productNumber = parseInt(productNumber) - 1;
+    }
+    productInput.value = productNumber;
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productNumber * price;
+};
+
+// ================iPhone 11 128GB Black=================
+// ================phone-plus===============
+document.getElementById('phone-plus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, true);
+})
+// ==============phone-minus=============
+document.getElementById('phone-minus').addEventListener('click', function () {
+    // console.log('caseTotal')
+    updateProductNumber('phone', 1219, false);
+})
+// =================iPhone 11 Silicone Case==============
+// ===============case-plus=============
 document.getElementById('case-plus').addEventListener('click', function () {
-    const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value;
-    caseInput.value = parseInt(caseNumber) + 1;
-});
+    updateProductNumber('case', 59, true);
+})
+// =============case-minus===============
 document.getElementById('case-minus').addEventListener('click', function () {
-    const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value;
-    caseInput.value = parseInt(caseNumber) - 1;
-    console.log(caseNumber);
-});
+    updateProductNumber('case', 59, false);
+})
