4ae3d5457cd0d564e64028b71c06cd75ec4a3e85
alamin0561464977 <amin23435678901315@gmail.com>
2022-02-13 21:22:36 +0300

    My shopping cart

--- app.js
+++ app.js
@@ -11,7 +11,25 @@ function updateProductNumber(product, price, isIncreaSing) {
     productInput.value = productNumber;
     const productTotal = document.getElementById(product + '-total');
     productTotal.innerText = productNumber * price;
+    calculateTotal();
 };
+
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+function calculateTotal() {
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59
+    const sumTotal = phoneTotal + caseTotal;
+    const Tax = sumTotal / 10;
+    const totalPrice = sumTotal + Tax;
+    document.getElementById('sub-total').innerText = sumTotal;
+    document.getElementById('Tax-amount').innerText = Tax;
+    document.getElementById('total-price').innerText = totalPrice;
+
+}
 
 // ================iPhone 11 128GB Black=================
 // ================phone-plus===============
